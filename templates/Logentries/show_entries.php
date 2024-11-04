dce09929fe8fddec7266d4cdfac9359fd9db2686
patrickakk <patrick@algority.eu>
2024-11-04 11:57:17 +0100

    simplified logging - view

--- templates/Logentries/show_entries.php
+++ templates/Logentries/show_entries.php
@@ -30,11 +30,9 @@
                 <tr>
                     <th align="left" style="padding: 5px"><?= $this->Paginator->sort('id') ?></th>
                     <th align="left" style="padding: 5px"><?= $this->Paginator->sort('logentry_code_id', 'Type') ?></th>
-                    <th align="left" style="padding: 5px"><?= $this->Paginator->sort('user_id') ?></th>
                     <th align="left" style="padding: 5px"><?= $this->Paginator->sort('source_name', 'Application Source') ?></th>
                     <th align="left" style="padding: 5px"><?= $this->Paginator->sort('subject', 'Action') ?></th>
                     <th align="left" style="padding: 5px"><?= $this->Paginator->sort('description', 'Details') ?></th>
-                    <th align="left" style="padding: 5px"><?= $this->Paginator->sort('cleared') ?></th>
                     <th align="left" style="padding: 5px"><?= $this->Paginator->sort('created', ['label' => 'Date & Time']) ?></th>
                 </tr>
             </thead>
@@ -43,19 +41,13 @@
                     <tr>
                         <td align="left" style="padding: 5px"><?= $this->Number->format($logentry->id) ?></td>
                         <td align="left" style="padding: 5px"><?= $logentry->logentry_code->id . ' - ' . $logentry->logentry_code->name ?></td>
-                        <td align="left" style="padding: 5px"><?= $this->Html->link(
-                                                                    $logentry->user->id,
-                                                                    ['controller' => 'Users', 'action' => 'view', $logentry->user->id],
-                                                                    ['title' => $logentry->user->first_name . ' ' . $logentry->user->last_name]
-                                                                ) ?></td>
                         <td align="left" style="padding: 5px"><?= h($logentry->source_name) ?></td>
                         <td align="left" style="padding: 5px"><?= h($logentry->subject) ?></td>
                         <td align="left" style="padding: 5px"><?= $this->Text->autoParagraph($logentry->description) ?></td>
-                        <td align="left" style="padding: 5px"><?= ($logentry->cleared ? "Yes" : "No") ?></td>
                         <td align="left" style="padding: 5px"><?= h($logentry->created->i18nFormat('yyyy-MM-dd HH:mm')) ?> UTC</td>
                     </tr>
                     <tr>
-                        <td colspan="8">
+                        <td colspan="6">
                             <hr>
                         </td>
                     </tr>
